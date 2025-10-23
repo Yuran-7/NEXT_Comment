ee7a0fca2ccbc845e213fc0888bcd4a50c6f0ac5
ysh <1724277428@qq.com>
2025-10-23 21:02:46 +0800

    哎

--- examples/secondary_index_data_write_num.cc
+++ examples/secondary_index_data_write_num.cc
@@ -281,7 +281,7 @@ int main(int argc, char* argv[]) {
 }
 
 /*
-g++ -g3 -O0 -std=c++17 \
+g++ -O3 -std=c++17 \
   -faligned-new -DHAVE_ALIGNED_NEW \
   -DROCKSDB_PLATFORM_POSIX -DROCKSDB_LIB_IO_POSIX \
   -DOS_LINUX -fno-builtin-memcmp \
@@ -293,9 +293,9 @@ g++ -g3 -O0 -std=c++17 \
   -march=native -DHAVE_SSE42 -DHAVE_PCLMUL -DHAVE_AVX2 \
   -DHAVE_BMI -DHAVE_LZCNT -DHAVE_UINT128_EXTENSION \
   -fno-rtti secondary_index_data_write_num.cc \
-  -o secondary_index_data_write_num ../librocksdb_debug.a \
+  -o secondary_index_data_write_num ../librocksdb.a \
   -I../include -I.. \
-  -lpthread -lrt -ldl -lsnappy -lgflags -lz -lbz2 -llz4 -lzstd -lnuma -ltbb
+  -lpthread -lrt -ldl -lsnappy -lgflags -lz -lbz2 -llz4 -lzstd -lnuma -ltbb -luring
  */
 
 
--- examples/secondary_index_read_num.cc
+++ examples/secondary_index_read_num.cc
@@ -216,7 +216,7 @@ int main(int argc, char* argv[]) {
 }
 
 /*
-g++ -g3 -O0 -std=c++17 \
+g++ -O3 -std=c++17 \
   -faligned-new -DHAVE_ALIGNED_NEW \
   -DROCKSDB_PLATFORM_POSIX -DROCKSDB_LIB_IO_POSIX \
   -DOS_LINUX -fno-builtin-memcmp \
@@ -228,9 +228,9 @@ g++ -g3 -O0 -std=c++17 \
   -march=native -DHAVE_SSE42 -DHAVE_PCLMUL -DHAVE_AVX2 \
   -DHAVE_BMI -DHAVE_LZCNT -DHAVE_UINT128_EXTENSION \
   -fno-rtti secondary_index_read_num.cc \
-  -o secondary_index_read_num ../librocksdb_debug.a \
+  -o secondary_index_read_num ../librocksdb.a \
   -I../include -I.. \
-  -lpthread -lrt -ldl -lsnappy -lgflags -lz -lbz2 -llz4 -lzstd -lnuma -ltbb
+  -lpthread -lrt -ldl -lsnappy -lgflags -lz -lbz2 -llz4 -lzstd -lnuma -ltbb -luring
  */
 
  // ./secondary_index_read_num /NV1/ysh/NEXT/examples/testdb 1000 /NV1/ysh/dataset/buildings_1m/buildings_1D_query_0.01
